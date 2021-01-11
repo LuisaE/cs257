59c023ac05ac890cfe9e1efbbb8623004603be44
Luisa Esosteguy <luisaescosteguy@hotmail.com>
2021-01-11 11:55:34 -0600

    Fix command-line syntax

--- books/books.py
+++ books/books.py
@@ -7,8 +7,8 @@ csv_file = csv.reader(open('books.csv', "r"), delimiter=",")
 
 def get_parsed_arguments():
 	parser = argparse.ArgumentParser(description='Process books.csv using a keyword from the title, an author, or a range of publication')
-	parser.add_argument('--book-search', '-bs', nargs=1, metavar='S', help='print a list of books whose titles contain the string S')
-	parser.add_argument('--author-search', '-as', nargs=1, metavar='S', help='print a list of authors whose names contain the string S')
+	parser.add_argument('--book-search', '-b', nargs=1, metavar='S', help='print a list of books whose titles contain the string S')
+	parser.add_argument('--author-search', '-a', nargs=1, metavar='S', help='print a list of authors whose names contain the string S')
 	parser.add_argument('--publish','-p', nargs=2, metavar='year', help='print a list of books published between the two years')
 	parsed_arguments = parser.parse_args()
 	return parsed_arguments
