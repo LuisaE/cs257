9c1dfb2fb36f1b86dd5db819f3d697cf41927f1c
Luisa Esosteguy <luisaescosteguy@hotmail.com>
2021-01-11 11:20:38 -0600

    Add publish function and import books.cvs

--- books/books.py
+++ books/books.py
@@ -1,6 +1,9 @@
 #Claire Williams and Luisa Escosteguy
 
 import argparse
+import csv
+
+csv_file = csv.reader(open('books.csv', "r"), delimiter=",")
 
 def get_parsed_arguments():
 	parser = argparse.ArgumentParser(description='Process books.csv using a keyword from the title, an author, or a range of publication')
@@ -10,4 +13,30 @@ def get_parsed_arguments():
 	parsed_arguments = parser.parse_args()
 	return parsed_arguments
 
-get_parsed_arguments()
+def publish(args):
+	year_start, year_end = args
+	year_start = int(year_start)
+	year_end = int(year_end)
+
+	#make sure the years are in the correct order
+	if year_start > year_end:
+		temp = year_start
+		year_start = year_end
+		year_end = temp
+	
+	#find the books published between year_start and year_end
+	for row in csv_file:
+		name_of_book = row[0]
+		year_of_publication = int(row[1])
+		if year_of_publication >= year_start and year_of_publication <= year_end:
+			print(name_of_book, "published in", year_of_publication)
+
+def main():
+	arguments = get_parsed_arguments()
+
+	if arguments:
+		if arguments.publish:
+			publish(arguments.publish)
+
+if __name__ == '__main__':
+    main()
