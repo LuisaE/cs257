dbb0c06d8b8a355647c27ae25de7644ff656079c
Luisa Esosteguy <luisaescosteguy@hotmail.com>
2021-01-11 09:28:53 -0600

    Update wording

--- books/books.py
+++ books/books.py
@@ -3,10 +3,10 @@
 import argparse
 
 def get_parsed_arguments():
-	parser = argparse.ArgumentParser(description='process books.csv using a keyword from the title, an author, or a range of publication')
-	parser.add_argument('--book-search', '-bs', nargs=1, metavar='keyword', help='the keyword to search for in the book titles')
-	parser.add_argument('--author-search', '-as', nargs=1, metavar='author', help='the author whose books to search for')
-	parser.add_argument('--publish','-p', nargs=2, metavar='year', help='the range of years the books were published between')
+	parser = argparse.ArgumentParser(description='Process books.csv using a keyword from the title, an author, or a range of publication')
+	parser.add_argument('--book-search', '-bs', nargs=1, metavar='S', help='print a list of books whose titles contain the string S')
+	parser.add_argument('--author-search', '-as', nargs=1, metavar='S', help='print a list of authors whose names contain the string S')
+	parser.add_argument('--publish','-p', nargs=2, metavar='year', help='print a list of books published between the two years')
 	parsed_arguments = parser.parse_args()
 	return parsed_arguments
 
