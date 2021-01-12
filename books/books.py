d674a563182caf55de8804801215e417ba792097
Luisa Esosteguy <luisaescosteguy@hotmail.com>
2021-01-11 19:55:07 -0600

    Fix formatting, add support edge cases, clean up

--- books/books.py
+++ books/books.py
@@ -4,75 +4,76 @@ import argparse
 import csv
 
 def get_parsed_arguments():
-        parser = argparse.ArgumentParser(description='Process books.csv using a keyword from the title, an author, or a range of publication')
-        parser.add_argument('--book', '-b', nargs=1, metavar='S', help='print a list of books whose titles contain the string S')
-        parser.add_argument('--author', '-a', nargs=1, metavar='S', help='print a list of authors whose names contain the string S')
-        parser.add_argument('--publication','-p', nargs=2, metavar='year', help='print a list of books published between the two years')
-        parsed_arguments = parser.parse_args()
-        return parsed_arguments
+	parser = argparse.ArgumentParser(description='Process books.csv using a keyword from the title, an author, or a range of publication')
+	parser.add_argument('--book', '-b', nargs=1, metavar='S', help='print a list of books whose titles contain the string S')
+	parser.add_argument('--author', '-a', nargs=1, metavar='S', help='print a list of authors whose names contain the string S')
+	parser.add_argument('--publication','-p', nargs=2, metavar='year', help='print a list of books published between the two years')
+	parsed_arguments = parser.parse_args()
+	return parsed_arguments
 
 def book(arg, books):
-        new_books = []
-        keyword = arg[0].lower()
-        for book in books: 
-                if book[0].lower().count(keyword) > 0:
-                        new_books.append(book)
-        return new_books
+	new_books = []
+	S = arg[0].lower()
+
+	for book in books: 
+		if book[0].lower().count(S) > 0:
+			new_books.append(book)
+	
+	return new_books
 
 def author(arg, books):
-        new_books = []
-        name = arg[0].lower()
-        for book in books:
-                if book[2].lower().count(name) > 0:
-                        new_books.append(book)
-        return new_books
+	new_books = []
+	S = arg[0].lower()
+
+	for book in books:
+		if book[2].lower().count(S) > 0:
+			new_books.append(book)
+
+	return new_books
 
 def publication(args, books):
-        new_books = []
-        year_start, year_end = args
-        year_start = int(year_start)
-        year_end = int(year_end)
-        
-        #make sure the years are in the correct order
-        if year_start > year_end:
-                temp = year_start
-                year_start = year_end
-                year_end = temp
+	new_books = []
+	year_start, year_end = args
+	year_start = int(year_start)
+	year_end = int(year_end)
 
-        #find the books not published between year_start and year_end and remove from list
-        for book in books:
-                year_of_publication = int(book[1])
-                if year_of_publication >= year_start and year_of_publication <= year_end:
-                        new_books.append(book)
-        return new_books
+	if year_start > year_end:
+		temp = year_start
+		year_start = year_end
+		year_end = temp
 
-def get_data():
-        books = []
-        data = csv.reader(open('books.csv', "r"), delimiter=",")
-        #adds the data to a python list so we can remove rows that we want to filter out
-        for row in data:
-                books.append(row)
-        return books
+	for book in books:
+		year_of_publication = int(book[1])
+		if year_of_publication >= year_start and year_of_publication <= year_end:
+			new_books.append(book)
 
-#temporary print method for functionality, modify for formatting
+	return new_books
+
 def print_books(books):
-        for book in books:
-                print(book)
+	if not books:
+		print("We are sorry. We cannot find any results that match your search criteria.")
+		exit()
+	
+	for book in books:
+		print('\t'.join(book))
 
 def main():
-        arguments = get_parsed_arguments()
-        books = get_data()
+	arguments = get_parsed_arguments()
+	books = csv.reader(open('books.csv', "r"), delimiter=",")
 
-        if arguments:
-                if arguments.book:
-                        books = book(arguments.book, books)
-                if arguments.author:
-                        books = author(arguments.author, books)
-                if arguments.publication:
-                        books = publication(arguments.publication, books)
+	if arguments:
+		if arguments.book:
+			books = book(arguments.book, books)
+		if arguments.author:
+			books = author(arguments.author, books)
+		if arguments.publication:
+			if arguments.publication[0].isnumeric() and arguments.publication[1].isnumeric():
+				books = publication(arguments.publication, books)
+			else:
+				print("Please, type numbers only")
+				exit()
 	
-	#create function that prints what is left in books. How to print nicely?
-        print_books(books)
+	print_books(books)
 
 if __name__ == '__main__':
-        main()
+	main()
