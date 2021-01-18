78893d7390a91eca1f82e7ae92f987bbd7e25b82
Luisa Esosteguy <luisaescosteguy@hotmail.com>
2021-01-18 12:24:21 -0600

    Fix naming, clean up

--- books/books.py
+++ books/books.py
@@ -2,6 +2,7 @@
 
 import argparse
 import csv
+import sys
 
 books = []
 
@@ -9,14 +10,14 @@ def get_parsed_arguments():
 	parser = argparse.ArgumentParser(description='Process books.csv using a keyword from the title, an author, or a range of publication')
 	parser.add_argument('--book', '-b', nargs=1, metavar='S', help='print a list of books whose titles contain the string S')
 	parser.add_argument('--author', '-a', nargs=1, metavar='S', help='print a list of authors whose names contain the string S')
-	parser.add_argument('--publication','-p', nargs=2, metavar='year', help='print a list of books published between the two years')
+	parser.add_argument('--publication','-p', nargs=2, type=int, metavar='year', help='print a list of books published between the two years')
 	parsed_arguments = parser.parse_args()
 	return parsed_arguments
 
-def search_books_by_title(arg):
+def filter_books_by_title(title):
 	global books
 	new_books = []
-	S = arg[0].lower()
+	S = title[0].lower()
 
 	for book in books: 
 		if book[0].lower().count(S) > 0:
@@ -24,10 +25,10 @@ def search_books_by_title(arg):
 	
 	books = new_books
 
-def search_books_by_author(arg):
+def filter_books_by_author(author):
 	global books
 	new_books = []
-	S = arg[0].lower()
+	S = author[0].lower()
 
 	for book in books:
 		if book[2].lower().count(S) > 0:
@@ -35,17 +36,12 @@ def search_books_by_author(arg):
 
 	books = new_books
 
-def search_books_by_publication(args):
+def filter_books_by_publication(years):
 	global books
 	new_books = []
-	year_start, year_end = args
-	year_start = int(year_start)
-	year_end = int(year_end)
-
-	if year_start > year_end:
-		temp = year_start
-		year_start = year_end
-		year_end = temp
+	
+	year_start = min(years)
+	year_end = max(years)
 
 	for book in books:
 		year_of_publication = int(book[1])
@@ -71,19 +67,15 @@ def print_books():
 def main():
 	global books
 	arguments = get_parsed_arguments()
-	books = csv.reader(open('books.csv', "r"), delimiter=",")
+	books = csv.reader(open("books.csv", "r"), delimiter=",")
 
 	if arguments:
 		if arguments.book:
-			search_books_by_title(arguments.book)
+			filter_books_by_title(arguments.book)
 		if arguments.author:
-			search_books_by_author(arguments.author)
+			filter_books_by_author(arguments.author)
 		if arguments.publication:
-			if arguments.publication[0].isnumeric() and arguments.publication[1].isnumeric():
-				search_books_by_publication(arguments.publication)
-			else:
-				print("Please type numbers only")
-				exit()
+			filter_books_by_publication(arguments.publication)
 				
 	if not (arguments.book or arguments.author or arguments.publication):
 		print("Please add a flag to search for books")
